ae519380d0c85095cb89235b438cd6e779f188f1
varun1452 <varungaudani80@gmail.com>
2023-09-28 23:08:59 +0530

    added

--- src/home.js
+++ src/home.js
@@ -3,17 +3,29 @@ import './home.css';
 import React from 'react'
 
 
-function home() {
+function Home() {
 
-   
-    return (
-        <div>
-           
-                <h1>hello</h1>
-            
 
-        </div>
-    );
+  return (
+    <div>
+      {/* navigation section  */}
+
+      <div className='navbar-nav'> 
+      <h2 href="$">TASK TRAKER</h2>
+    
+      <a className="nav-link" href="$">Home</a>
+      <a className="nav-link" href="$">Contact Us</a>
+      <a className="nav-link" href="$">To do list</a>
+      </div>
+      {/* navigation section end */}
+
+      {/* TO do list section */}
+      <div></div>
+      {/* TO do list section */}
+
+
+    </div>
+  );
 }
 
-export default home;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import home from './home';
+import Home from './home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <home />
+    <Home />
   </React.StrictMode>
 );
 
