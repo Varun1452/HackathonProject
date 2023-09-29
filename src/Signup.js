21be83969e381fb6c371a531e257f13ca925fb7f
kaushalvaghela <kaushalvaghela2004@gmail.com>
2023-09-30 02:48:51 +0530

    new consept aaded

--- src/Signup.js
+++ src/Signup.js
@@ -1,33 +1,37 @@
-import { React } from 'react';
+ import React, {useState} from 'react'
+
 import './Signup.css'
 
-function Signup() {
+  function SignUp () {
+
+
   return (
     <body>
 
-    <div className='mainform'>
+    <div className='mainContainer'>
       <form>
 
-        <div className='mainContainer'>
+        <div className='mainform'>
         <h2>SignUp</h2>
 
-          <label>UserName</label> 
+          <label className='mainlabel'>UserName</label> 
           <input type='text'  className='maininput' /><br />
-          <label>Email</label>
+          <label className='mainlabel'>Email</label><br/>
           <input type='text'  className='maininput' /><br />
-          <label>PhoneNo</label>
-          <input type='text' className='maininput' /><br />
-          <label>Password</label>
+          <label className='mainlabel'>PhoneNo</label>
+          <input type='text' className='maininput'/><br/>
+          <label className='mainlabel'>Password</label>
           <input type='Password'  className='maininput' /><br />
-          <label>Confirm Password</label>
-          <input type='Password'  className='maininput' /><br />
- 
-          <button className='btn'>SignUp</button>
+          <label className='mainlabel'>Confirm Password</label>
+          <input type='Password'  className='maininput'/><br />
+
+          <button type=''>SignUp</button>
         </div >
       </form>
       </div>   
 
-    </body >
+    </body>
   );
 }
-export default Signup;
+export default SignUp
+;
